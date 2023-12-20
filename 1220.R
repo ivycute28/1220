95f54bb2bdedfdb787fbf1982de40132f3f2e107
le402n25\uer <hwlivy@gmail.com>
2023-12-20 16:55:03 +0800

    使控制區的參數隨著Server端的狀況而改變

--- 1220.R
+++ 1220.R
@@ -5,3 +5,11 @@ summary(Data)
 
 Color = "red"
 plot(Data, col = Color)
+
+Data = read.table("Example.txt", header = TRUE) #讀取Example.txt，並存成"Data"這個物件(資料表格式)
+
+Color = "red"          #指定顏色
+
+pdf("plot.pdf")      #開啟一個pdf file，路徑為"plot.pdf"
+plot(Data, col = Color) #將圖畫到開啟中的pdf file上去
+dev.off()            #關掉這個pdf file
